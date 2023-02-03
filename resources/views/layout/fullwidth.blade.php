99aa7afda5242ad50d93455b5895196ace36c11b
alexsandro.azevedo <alexsandro_gta@hotmail.com>
2023-02-03 17:24:21 -0300

    Uema logo no icone da pagina e login

--- resources/views/layout/fullwidth.blade.php
+++ resources/views/layout/fullwidth.blade.php
@@ -16,7 +16,7 @@
     <title>{{ config('dz.name') }} | @yield('title', $page_title ?? '')</title>
     <meta name="description" content="@yield('page_description', $page_description ?? '')"/>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/uema/icon_uema.png') }}">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     
