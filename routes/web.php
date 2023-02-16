f552e699728dd4db299c46f89b399926925d9ee5
Antonio <antoniojunior11@aluno.uema.br>
2023-02-16 14:50:49 -0300

    refactor: retirada da rota dash

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ZenixadminController;
-use App\Http\Controllers\DashController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/home', [HomeController::class,'index'])->name('home');
-
-Route::get('/', [DashController::class, 'index']);
+Route::get('/', [HomeController::class,'index'])->name('home');
 
 Route::prefix('tema')->group(function () {
     Route::get('/',             [ZenixadminController::class,'page_login'])->name('teste');
