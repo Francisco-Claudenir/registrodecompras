e9c2d4f09c5793dc0d7f9d45e1691a07499af56a
alexsandro.azevedo <alexsandro_gta@hotmail.com>
2023-02-27 09:31:22 -0300

    admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    
     public function run()
     {
         $user = new User();
         $user->name = 'Administrador';
-        $user->email = 'admin@email.com';
+        $user->usuario = 'admin@email.com';
         $user->password = Hash::make('ctic@uema');
+
+        User::create(['name' => 'Administrador', 'usuario' => 'admin@email.com', 'password' => Hash::make('ctic@uema')]);
     }
 }
