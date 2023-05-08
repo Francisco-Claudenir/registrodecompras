fb0f2c2ffb4c1486348b6a2c5187ca8c486582ef
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-05-08 14:41:18 -0300

    feat: Migrations, Models, Controllers, RequestStore e RequestUpdate de Modalidade Bolsas e Plano Trabalho

---
+++ app/Http/Controllers/ModalidadeBolsaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreModalidadeBolsaRequest;
+use App\Http\Requests\UpdateModalidadeBolsaRequest;
+use App\Models\ModalidadeBolsa;
+
+class ModalidadeBolsaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreModalidadeBolsaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreModalidadeBolsaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ModalidadeBolsa  $modalidadeBolsa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModalidadeBolsa $modalidadeBolsa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ModalidadeBolsa  $modalidadeBolsa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModalidadeBolsa $modalidadeBolsa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateModalidadeBolsaRequest  $request
+     * @param  \App\Models\ModalidadeBolsa  $modalidadeBolsa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateModalidadeBolsaRequest $request, ModalidadeBolsa $modalidadeBolsa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ModalidadeBolsa  $modalidadeBolsa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModalidadeBolsa $modalidadeBolsa)
+    {
+        //
+    }
+}
---
+++ app/Models/PlanoTrabalho.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlanoTrabalho extends Model
+{
+    use HasFactory;
+}
