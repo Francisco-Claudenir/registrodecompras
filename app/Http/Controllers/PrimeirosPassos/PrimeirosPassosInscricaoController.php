3a73f09c7fb4100980b07973938d8f909b833672
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-05-12 17:47:27 -0300

    feat: Configurações iniciais da controller de Primeiros Passos

--- app/Http/Controllers/PrimeirosPassos/PrimeirosPassosInscricaoController.php
+++ app/Http/Controllers/PrimeirosPassos/PrimeirosPassosInscricaoController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\PrimeirosPassos;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StorePrimeirosPassosInscricaoRequest;
-use App\Http\Requests\UpdatePrimeirosPassosInscricaoRequest;
+use App\Http\Requests\PrimeirosPassos\UpdatePrimeirosPassosInscricaoRequest;
+use App\Http\Requests\PrimeirosPassos\StorePrimeirosPassosInscricaoRequest;
 use App\Models\PrimeirosPassosInscricao;
 
 class PrimeirosPassosInscricaoController extends Controller
 {
+    protected $primeirospassosinscricao;
+    protected $bag = [
+        'view' => '',
+        'route' => '',
+        'Title' => '',
+        'subtitle' => '',
+        'msg' => 'temauema.msg.register'
+    ];
+
+    public function __construct(PrimeirosPassosInscricao $ppinscricao)
+    {
+        $this->ppinscricao = $ppinscricao;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -16,7 +30,7 @@ class PrimeirosPassosInscricaoController extends Controller
      */
     public function index()
     {
-        //
+        return view('teste');
     }
 
     /**
@@ -26,13 +40,12 @@ class PrimeirosPassosInscricaoController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StorePrimeirosPassosInscricaoRequest  $request
+     * @param  \App\Http\Requests\PrimeirosPassos\StorePrimeirosPassosInscricaoRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(StorePrimeirosPassosInscricaoRequest $request)
@@ -65,7 +78,7 @@ class PrimeirosPassosInscricaoController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdatePrimeirosPassosInscricaoRequest  $request
+     * @param  \App\Http\Requests\PrimeirosPassos\UpdatePrimeirosPassosInscricaoRequest  $request
      * @param  \App\Models\PrimeirosPassosInscricao  $primeirosPassosInscricao
      * @return \Illuminate\Http\Response
      */
