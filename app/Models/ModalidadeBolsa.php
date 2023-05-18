f113fa13f046566c8d388e96a4b999d05fdbdede
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-05-18 15:36:38 -0300

    feat: Models modalidade, plano trabalho. Relacionamento entre as tabelas e criação da tabela pivot

--- app/Models/ModalidadeBolsa.php
+++ app/Models/ModalidadeBolsa.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ModalidadeBolsa extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['nome'];
+
+    protected $primaryKey = 'modalidade_id';
+
+    protected $dates = ['created_at','deleted_at'];
+
+
+    public function planostrabalhos()
+    {
+        return $this->hasMany(PlanoTrabalho::class,'modalidade_id')->withTrashed();
+    }
 }
