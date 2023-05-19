9f0db97a3309c2674116ef9ed54808eb985ab564
FRC1995 <ffranciscosantos1995@gmail.com>
2023-05-19 17:11:58 -0300

    feat: Adicao da views edit, index e uma pequena alteracao no view create de GrandeArea

---
+++ resources/views/admin/grandearea/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.page', [
+    'layout' => 'admin',
+])
+
+@section('title', ' - Index GrandArea')
+
+@section('content')
+
+<div class="container-fluid">
+    <!-- Add Project -->
+    <div class="modal fade" id="addProjectSidebar">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Create Project</h5>
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label class="text-black font-w500">Project Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label class="text-black font-w500">Deadline</label>
+                            <input type="date" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label class="text-black font-w500">Client Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn btn-primary">CREATE</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row page-titles mx-0">
+        <div class="col-sm-6 p-md-0">
+            <div class="welcome-text">
+                <h4>Olá, seja bem vindo!</h4>
+            </div>
+        </div>
+    </div>
+    <!-- row -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Lista GrandeArea</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="example5" class="table table-responsive-sm" style="min-width: 845px">
+                            <thead>
+                                <tr>
+                                    <th><h5>Name</h5></th>
+                                    <th><h5>Action</h5></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($grandeareas as $grandearea )
+                                <tr>
+                                    <td><p>{{$grandearea->nome }}</p></td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="{{ route('grandearea.edit', ['grandearea' =>$grandearea->area_id]) }}" class="btn btn-primary shadow btn-xs sharp me-1"><i class="fas fa-pencil-alt"></i></a>
+                                        </div>												
+                                    </td>												
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
