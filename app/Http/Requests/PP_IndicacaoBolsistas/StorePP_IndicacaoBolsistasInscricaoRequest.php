e8b24826a6689afc5c819ba35f227acbba17e24a
daniel.sousa <danieloliveirasousaa@gmail.com>
2023-05-24 15:56:23 -0300

    feat: Adicao de PP_IndicacaoBolsistaInscricao, Controller e Resquest Store

--- app/Http/Requests/PP_IndicacaoBolsistas/StorePP_IndicacaoBolsistasInscricaoRequest.php
+++ app/Http/Requests/PP_IndicacaoBolsistas/StorePP_IndicacaoBolsistasInscricaoRequest.php
@@ -13,7 +13,7 @@ class StorePP_IndicacaoBolsistasInscricaoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,26 @@ class StorePP_IndicacaoBolsistasInscricaoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'curso' => ['required', 'string', 'min:2', 'max:200'],
+            'centro' => ['required', 'string', 'min:2', 'max:200'],
+            'numero_identidade' => ['required', 'string', 'min:2', 'max:200'],
+            'documento_identidade' => ['required'],
+            'documento_cpf' => ['required'],
+            'nome_orientador' => ['required', 'string', 'min:2', 'max:150'],
+            'telefone_orientador' => ['required', 'string', 'min:2', 'max:100'],
+            'email_orientador' => ['required', 'string', 'min:2', 'max:100'],
+            'titulo_projeto_orientador' => ['required', 'string', 'min:2', 'max:200'],
+            'titulo_plano_orientador' => ['required', 'string', 'min:2', 'max:200'],
+            'historico_escolar' => ['required'],
+            'declaracao_vinculo' => ['required'],
+            'termo_compromisso_bolsista' => ['required'],
+            'declaracao_negativa_vinculo' => ['required'],
+            'curriculo' => ['required'],
+            'declaracao_conjuta_estagio' => ['nullable'],
+            'agencia_banco' => ['required', 'string', 'min:2', 'max:100'],
+            'numero_conta_corrente' => ['required', 'string', 'min:2', 'max:100'],
+            'comprovante_conta_corrente' => ['required'],
+            'termo_compromisso_orientador' => ['required']
         ];
     }
 }
