3650f4cdd0de1ad52fa48de4615138b10f44b127
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-06-01 18:32:00 -0300

    feat: Migration, Model, Create e index de Perfil

---
+++ app/Http/Controllers/PerfilController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Perfil;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PerfilController extends Controller
+{
+    protected $perfil;
+    protected $bag = [
+        'view' => 'admin.perfil',
+        'route' => 'perfil',
+        'Title' => '',
+        'subtitle' => '',
+        'msg' => 'temauema.msg.register'
+    ];
+
+    public function __construct(Perfil $perfil)
+    {
+        $this->perfil = $perfil;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $perfis = $this->perfil->get();
+        return view($this->bag['view'] . '.index', compact('perfis'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view($this->bag['view'] . '.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+            $this->perfil->create($request->all());
+            DB::commit();
+            alert()->success(config($this->bag['msg'] . '.success.create'));
+            return redirect()->route('perfil.index');
+        } catch (\Throwable $th) {
+            dd($th);
+            DB::rollBack();
+            alert()->error(config($this->bag['msg'] . '.error.create'));
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Perfil  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Perfil $perfil)
+    {
+        //
+    }
+}
