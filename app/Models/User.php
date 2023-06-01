bb13d8f4babcefa8f11847d0ab7a816471abf89a
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-06-01 18:33:09 -0300

    feat: Controller, request, models, create e index de User

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'nome', 'email','cpf','telefone','endereco', 'password',
+        'nome', 'email', 'cpf', 'telefone', 'endereco', 'password', 'perfil_id'
     ];
-     protected $casts = [
+    protected $casts = [
         'endereco' => 'array'
-     ];
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -35,5 +35,16 @@ class User extends Authenticatable
     {
         dd("gfdgsdgfds");
         return $this->hasMany(PP_IndicacaoBolsistasInscricao::class, 'id')->withTrashed();
+    }
+
+    public function perfil()
+    {
+        return $this->belongsTo(Perfil::class, 'perfil_id');
+    }
+
+    public function hasPerfil(string $perfil)
+    {
+        $perfilAtual = $this->perfil;
+        return $perfilAtual && $perfilAtual->nome == $perfil;
     }
 }
