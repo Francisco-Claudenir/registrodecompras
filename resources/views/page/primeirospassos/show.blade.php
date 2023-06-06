22bb40e71810e163b338909b1a724131ca56d417
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-06-06 11:16:32 -0300

    fix: Correção do bug do footer

--- resources/views/page/primeirospassos/show.blade.php
+++ resources/views/page/primeirospassos/show.blade.php
@@ -187,18 +187,6 @@
             </div>
         </div>
     </div>
-    <div class=" footer">
-        <div class="copyright">
-            <p>
-                Todos os direitos reservados Universidade Estadual do Maranhão -
-                <a href="https://www.uema.br/" target="_blank">UEMA</a> {{ now()->year }}
-            </p>
-            <p>
-                Coordenação de Tecnologia da Informação e Comunicação -
-                <a href="https://ctic.uema.br/" target="_blank">CTIC</a>
-            </p>
-        </div>
-    </div>
 @endsection
 
 @section('css')
