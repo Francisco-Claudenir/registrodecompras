447df1dd28a4a06814828cc1f728d9d6c6a9b1e2
daniel.sousa <danieloliveirasousaa@gmail.com>
2023-06-06 16:12:04 -0300

    feat: Adicao de validacao de data inicio e fim do evento

--- app/Models/PrimeiroPasso.php
+++ app/Models/PrimeiroPasso.php
@@ -35,6 +35,11 @@ class PrimeiroPasso extends Model
         $quanto_passou = $today->diffInDays($inicio);
         $fim = Carbon::createFromFormat('Y-m-d H:i:s', $this->data_fim);
         $total_dias = $fim->diffInDays($inicio) + 1;
+
+        if(($quanto_passou / $total_dias) * 100 > 100.00){
+            return 100.00;
+        }
+        
         return ($quanto_passou / $total_dias) * 100;
     }
 }
--- resources/views/admin/primeirospassos/create.blade.php
+++ resources/views/admin/primeirospassos/create.blade.php
@@ -44,7 +44,7 @@
                                     <label class="form-label">Data Inicio</label>
                                     <input type="date"
                                         class="form-control @if ($errors->first('data_inicio')) is-invalid @endif"
-                                        name="data_inicio" required="" value="{{ old('data_inicio') }}">
+                                        name="data_inicio" required="" value="{{ old('data_inicio') }}" id="data_inicio">
                                     @if ($errors->has('data_inicio'))
                                         <div class="invalid-feedback">{{ $errors->first('data_inicio') }}</div>
                                     @endif
@@ -53,7 +53,7 @@
                                     <label class="form-label">Data Fim</label>
                                     <input type="date"
                                         class="form-control @if ($errors->first('data_fim')) is-invalid @endif"
-                                        name="data_fim" required="" value="{{ old('data_fim') }}">
+                                        name="data_fim" required="" value="{{ old('data_fim') }}" id="data_fim">
                                     @if ($errors->has('data_fim'))
                                         <div class="invalid-feedback">{{ $errors->first('data_fim') }}</div>
                                     @endif
@@ -78,4 +78,32 @@
             </div>
         </div>
     </div>
+    <script>
+        // Obtém a data atual
+        var today = new Date();
+
+        // Obtém o dia, mês e ano da data atual
+        var dd = today.getDate();// dia
+        var mm = today.getMonth() + 1; //January is 0! // mês (lembrando que janeiro é representado por 0)
+        var yyyy = today.getFullYear();// ano
+
+        // Verifica se o dia é menor que 10 e adiciona um zero à frente, se necessário
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+
+        // Verifica se o mês é menor que 10 e adiciona um zero à frente, se necessário
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+
+        // Formata a data no formato "yyyy-mm-dd"
+        today = yyyy + '-' + mm + '-' + dd;
+
+        // Define a data mínima nos elementos HTML com os IDs "data_inicio" e "data_fim"
+        document.getElementById("data_inicio").setAttribute("min", today);
+        document.getElementById("data_fim").setAttribute("min", today);
+    </script>
+@endsection
+@section('scripts')
 @endsection
