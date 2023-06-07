309d74bd947d7bbf21988332f9dccf7d8e9f4e87
FRC1995 <ffranciscosantos1995@gmail.com>
2023-06-07 16:50:55 -0300

    refactor: adicao do titulo no create e index users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,17 +10,22 @@
         <div class="row page-titles mx-0">
             <div class="col-sm-6 p-md-0">
                 <div class="welcome-text">
-                    <h4>Olá, seja bem vindo!</h4>
+    
+                    <h4 class="card-title">Lista Usuários</h4>
+    
                 </div>
+            </div>
+            <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="">Usuários</a></li>
+                    <li class="breadcrumb-item active"><a href="">Lista</a></li>
+                </ol>
             </div>
         </div>
         <!-- row -->
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Lista Perfis</h4>
-                    </div>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table id="example5" class="table table-responsive-sm" style="min-width: 845px">
