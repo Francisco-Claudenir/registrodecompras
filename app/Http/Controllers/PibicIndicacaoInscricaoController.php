d94d5b3f739941f7308671827a3bc4f4bd292ba6
Ferreira Junior <joseferreira@proinfra.uema.br>
2023-07-14 12:40:43 -0300

    feat: Controllers de Pibic indicação e Pibic Indicação Inscrição

--- app/Http/Controllers/PibicIndicacaoInscricaoController.php
+++ app/Http/Controllers/PibicIndicacaoInscricaoController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Centro;
+use App\Models\PibicIndicacao;
 use App\Models\PibicIndicacaoInscricao;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PibicIndicacaoInscricaoController extends Controller
 {
+    protected $pibicIndicacao, $pibicIndicacaoInscricao, $user, $centros;
+    protected $bag = [
+        'view' => 'page.pibic_indicacao',
+        'route' => 'pibicindicacao',
+        'msg' => 'temauema.msg.register'
+    ];
+    public function __construct(PibicIndicacao $pibicIndicacao, PibicIndicacaoInscricao $pibicIndicacaoInscricao, User $user, Centro $centros)
+    {
+        $this->pibicIndicacao = $pibicIndicacao;
+        $this->pibicIndicacaoInscricao = $pibicIndicacaoInscricao;
+        $this->centros = $centros;
+        $this->user = $user;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -22,8 +39,24 @@ class PibicIndicacaoInscricaoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($pibicindicacao_id)
     {
+        // dd($pibicindicacao_id);
+
+        $data_hoje = Carbon::now();
+
+        $pibics = $this->pibicIndicacao->findOrfail($pibicindicacao_id);
+
+        if (($data_hoje->gte($pibics->data_inicio) && $data_hoje->lte($pibics->data_fim))) {
+
+            $centros = $this->centros->where('centros', 'not like', "%Polo%")->get();
+
+            return view('page.pibic_indicacao.create', compact('pibics', 'centros'));
+        } else {
+
+            alert()->error(config($this->bag['msg'] . '.error.data_inscricao'));
+            return redirect()->back();
+        }
         //
     }
 
@@ -44,8 +77,30 @@ class PibicIndicacaoInscricaoController extends Controller
      * @param  \App\Models\PibicIndicacaoInscricao  $pIndicacaoInscricao
      * @return \Illuminate\Http\Response
      */
-    public function show(PibicIndicacaoInscricao $pIndicacaoInscricao)
+    public function show($pibicindicacao_id, $user_id, Request $request)
     {
+
+        //Verificando se o primeiropasso_id existe
+        $this->pibicIndicacao->findOrfail($pibicindicacao_id);
+
+
+        //Verificando se o user_id existe
+        $this->user->findOrfail($user_id);
+
+        $dadosInscrito = $this->pibicIndicacaoInscricao->with('pp_inscricao_subArea', 'pp_inscricao_subArea.subArea_grandeArea', 'centros', 'planotrabalho', 'pp_inscricao_user')
+            ->join('users', 'users.id', '=', 'primeiros_passos_inscricaos.user_id')
+            ->where('users.id', '=', $user_id)
+            ->where('primeiros_passos_inscricaos.primeiropasso_id', '=', $pibicindicacao_id)
+            ->paginate(10);
+
+        foreach ($dadosInscrito as $key => $dados) {
+
+            $dados->endereco = json_decode($dados->endereco, true);
+        }
+
+        // dd($dadosInscrito);
+        $links = $dadosInscrito->appends($request->except('page'));
+        return view('page.pibic_indicacao.show', compact('dadosInscrito', 'links'));
         //
     }
 
